f90870de9dd7fcbae95b8c5c735f167679acfa07
rulkimi <azrulkimi00@gmail.com>
2024-02-28 23:58:06 +0800

    include sass to the project to replace primary colors

--- plugins.js
+++ plugins.js
@@ -2,15 +2,3 @@ export const $viewport = {
   isMobileView: () => window.innerWidth <= 767,
   isMobileAndTabletView: () => window.innerWidth <= 1024,
 };
-
-// Function to handle viewport change
-function handleViewportChange() {
-  console.log('Is Mobile View:', $viewport.isMobileView());
-  console.log('Is Mobile and Tablet View:', $viewport.isMobileAndTabletView());
-}
-
-// Event listener for viewport change
-window.addEventListener('resize', handleViewportChange);
-
-// Initial check on page load
-window.addEventListener('load', handleViewportChange);
